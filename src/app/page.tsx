636ed2cdda76798d454af5369cd2dcd0928484f5
Sam Louloudis <samanthaleelouloudis@gmail.com>
2023-11-26 11:53:55 +0000

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image'
 import { sql } from '@vercel/postgres'
 
+// dont let it cache. (will disable.)
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
-  // Ensure that the column name 'Views' is correctly referenced with its case
 
-  // Insert a row if the table is empty
   await sql`INSERT INTO views (Views) SELECT 0 WHERE NOT EXISTS (SELECT * FROM views)`;
-
-  // Increment the Views count
   await sql`UPDATE views SET Views = Views + 1`;
-
-  // Retrieve the updated Views count
   const result = await sql`SELECT Views FROM views`;
+  
+  // inesrt row if table is empty
+  // inc views
+  // get views
 
+  // checkout what we get back
   console.log(result)
 
-  // Check if the result is not empty and has the expected structure
-  // const views = result.length > 0 ? result[0].Views : 0;
 
   return (
     <div>
@@ -25,3 +25,4 @@ export default async function Home() {
     </div>
   )
 }
+
